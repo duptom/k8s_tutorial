700a5c50ed354bd766030e9b6fb987354fc038fe
Tommy <duptom4@gmail.com>
2019-11-22 20:27:40 -0500

    Ne pas lancer le init dans l'étape 1.

--- 1-install.sh
+++ 1-install.sh
@@ -49,10 +49,3 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 systemctl daemon-reload
 systemctl restart kubelet
-
-
-##-------------------------------------------------
-## Démarrer kubeadm avec la configuration de Cilium
-##-------------------------------------------------
-sudo swapoff -a
-kubeadm init --pod-network-cidr=10.217.0.0/16
