3e207cff01527fcb1be9f92a2f4f0899e954a73d
Tommy <duptom4@gmail.com>
2019-11-22 20:56:43 -0500

    Tester une installation avec microk8s.

---
+++ new_install.sh
@@ -0,0 +1,13 @@
+##------------------------------------------
+# Install snap
+##------------------------------------------
+sudo apt install snapd
+
+##------------------------------------------
+# Install microk8s
+##------------------------------------------
+sudo snap install microk8s --classic --channel=1.16/stable
+
+## Afficher le status de microk8s
+microk8s.status --wait-ready
+
